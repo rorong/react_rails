ce48dd64b2038c0a3ea3521182e4f61b850a9052
rordev <rordev@ongraph.com>
2018-08-29 14:59:04 +0530

    crud of users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,29 +1,60 @@
 class Api::V1::UsersController < ApplicationController
 
+  before_action :authenticate_api_user, only: :update_user
+  before_action :selected_user, only: %w(update show)
+
   def create
     if params[:user][:role].eql? "admin"
       @user = Admin.new(user_params)
     else
       @user = User.new(user_params)
     end
-      @user.password_hash = user_params[:password]
-      if @user.save
-        render json: @user
-      else
-       render json: {message: @user.try(:errors).try(:full_messages)}, status: :bad_request
-      end
+    @user.password_hash = user_params[:password]
+    if @user.save
+      render json: @user
+    else
+      render json: {message: @user.try(:errors).try(:full_messages)}, status: :bad_request
+    end
   end
 
   def login
-    @user = User.find_by_email(user_params[:email])
-    if @user.present? && @user.authenticate(user_params[:password])
-      render json: @user
+    user = User.find_by_email(user_params[:email])
+    if user.present? && user.authenticate(user_params[:password])
+      if user.admin?
+        render json: {users: User.where(role: 'user')}
+      else
+        render json: {users: user}
+      end
     else
       render json: {message: "Invalid credentials!"}, status: :unauthorized
     end
   end
 
+
+  def show
+    render json: @user
+  end
+
+  def update
+    if @user.present? && @user.update(user_params)
+      render json: {users: User.where(role: 'user')}
+    end
+  end
+
+  def update_user
+    if @current_api_user.update(user_params)
+      render json: @current_api_user
+    end
+  end
+
+  def index
+  end
+
   private
+
+  def selected_user
+    @user = User.find_by(id: params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:full_name, :email, :password, :role)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
-skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+
+  def authenticate_api_user
+    begin
+      puts request.headers
+      @current_api_user = User.find_by_token(request.headers["HTTP_APISECRET"])
+      raise "user_not_found" if @current_api_user.nil?
+    rescue Exception => @e
+      puts "error_exception #{Time.now} #{@e.message}"
+      err_hash={}
+      err_hash[:error]=@e.message
+      status = :unauthorized
+      render json: err_hash.to_json, status: status
+    end
+  end
 
 end
