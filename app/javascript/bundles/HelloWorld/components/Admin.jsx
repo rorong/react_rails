215b8afef6a3e561ecd3b914d8cf82ba38ac79d3
rorong <rordev@ongraph.com>
2018-09-07 19:02:39 +0530

    Update Admin.jsx

--- app/javascript/bundles/HelloWorld/components/Admin.jsx
+++ app/javascript/bundles/HelloWorld/components/Admin.jsx
@@ -11,6 +11,7 @@ class Admin extends React.Component {
   };
   constructor(props) {
     super(props);
+    // Initially state of email, Password and name
     this.state = {
      email : '',
      password: '',
@@ -18,40 +19,47 @@ class Admin extends React.Component {
     }
   }
 
+   //Submitting the handle form
     handleSubmit = () => {
      const {email, name, password} = this.state;
       const checkValidation = this.validateForm();
-  }
-
+   }
+   //For Validating the Form
     validateForm () {
      const State = this.state;
      const PwdValidation = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{6,50}$/;
      const EmailValidation = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
+   //validate the blank check for name property
     if(this.state.name === "") {
       alert("define name")
     }
+   //validate the length of the name property
     else if(this.state.name && this.state.name.length < 4 ) {
       alert("length problem")
     }
+    //validate the blank check for email property
     else if(this.state.email === "") {
       alert("email define")
     }
+    //Email Validation check       
     else if(!EmailValidation.test(this.state.email)) {
       alert("Please fill the correct email!")
     }
+    //Password Validation Check
     else if(!PwdValidation.test(this.state.password)) {
       alert("Password should be more than 6 characters with letters, numbers, special character")
-    }
+    }  
     else if(State.email != '' && State.name!= '' && State.password != '' ) {
       const OBJ={ 'email': this.state.email,'full_name': this.state.name, 'password': this.state.password , 'role': 'admin'}
       const ADMIN = {'user' : OBJ }
+    //dispatch the signup action
       this.props.dispatch(signup(ADMIN))
       alert("CREATED")
       this.setState({name: '', email: '', password: ''})
     }
   }
 
+  // Redirect to Login Screen
   handleLogin() {
     this.props.history.push('/hello_world/')
   }
@@ -113,12 +121,14 @@ class Admin extends React.Component {
   }
 }
 
+// Fetch Redux state and use as a props in component
 const mapStateToProps = (state) => {
     return {
       formData : state.formData
     };
   }
 
+// Define PropTypes
 Admin.propTypes = {
   cookies: instanceOf(Cookies).isRequired
 };
